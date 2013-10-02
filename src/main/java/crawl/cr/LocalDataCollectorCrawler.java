b51a7802985ce43409a5f8de90fe03ead2e93161
Sarp Kaya <sarpkaya@live.com.au>
2013-10-03 00:21:12 +1000

    saved file name gets encoded, which prevents +, = and other invalid file characters

--- src/main/java/crawl/cr/LocalDataCollectorCrawler.java
+++ src/main/java/crawl/cr/LocalDataCollectorCrawler.java
@@ -22,6 +22,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -31,7 +34,7 @@ import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
 
 public class LocalDataCollectorCrawler extends WebCrawler {
-	String downloadFolder = "C:/crawl/download";
+	String downloadFolder = "C:/crawl/download/";
 	Pattern filters = Pattern.compile(".*(\\.(css|js|bmp|gif|jpe?g" + "|png|tiff?|mid|mp2|mp3|mp4"
 			+ "|wav|avi|mov|mpeg|ram|m4v|pdf" + "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
 
@@ -58,7 +61,28 @@ public class LocalDataCollectorCrawler extends WebCrawler {
 			myCrawlStat.incTotalLinks(links.size());
 			
 			String content = parseData.getText();
-			String downloadPath = downloadFolder + page.getWebURL().getPath() + ".txt";
+			URL addressURL = null;
+			try {
+				addressURL = new URL(page.getWebURL().toString());
+			} catch (MalformedURLException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			}
+			
+			String urlSubs[] = addressURL.getFile().toString().split("/");
+			String urlNotEncoded = addressURL.getHost();
+			for (int i = 0; i < urlSubs.length-1; i++) {
+				urlNotEncoded += urlSubs[i] + "/";
+			}
+			
+			String pathEncoded = urlSubs[urlSubs.length-1];
+			try {
+				pathEncoded = URLEncoder.encode(urlSubs[urlSubs.length-1], "UTF-8");
+			} catch (UnsupportedEncodingException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			String downloadPath = downloadFolder + urlNotEncoded + pathEncoded + ".txt";
 			
 			File f = new File(downloadPath);
 			f.getParentFile().mkdirs();
@@ -73,12 +97,12 @@ public class LocalDataCollectorCrawler extends WebCrawler {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			String searchIt = "Engineering";
+			System.out.println("Downloaded path is " + downloadPath);
+			System.out.println("URL orig is " + page.getWebURL());
+			/*String searchIt = "Engineering";
 			if (content.toLowerCase().contains(searchIt.toLowerCase())) {
 				System.out.println(searchIt + " is found on " + page.getWebURL().getURL() );
-			}
-			
+			}*/
 			try {
 				myCrawlStat.incTotalTextSize(parseData.getText().getBytes("UTF-8").length);
 			} catch (UnsupportedEncodingException ignored) {
